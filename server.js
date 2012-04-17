a799e1f7c8032977c7b1a71bc6cb4585561eef86
Paul Sweeney <paul@8-bitdesign.com>
2012-04-16 23:44:32 -0700

    Updating port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.configure('production', function(){
 
 require('./routes/index')(app);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log(
   "Express server listening on port %d in %s mode",
   app.address().port,
